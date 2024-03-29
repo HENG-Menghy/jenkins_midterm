bac01610c14c03438cbe622ab953fcaf8d4120ae
HENG-Menghy <hengmenghy2002@gmail.com>
2024-03-29 16:08:04 +0700

    ert

---
+++ src/main/java/DevOps/midterm/demo_midterm/DemoMidtermApplication.java
@@ -0,0 +1,13 @@
+package DevOps.midterm.demo_midterm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMidtermApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMidtermApplication.class, args);
+	}
+
+}
---
+++ src/main/java/DevOps/midterm/demo_midterm/ServletInitializer.java
@@ -0,0 +1,13 @@
+package DevOps.midterm.demo_midterm;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoMidtermApplication.class);
+	}
+
+}
